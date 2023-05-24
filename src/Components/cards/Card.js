3c9402da3d270d2ee9d4a54b5ffdb563726a8b62
Raimbault Antoine <raimbaultantoine1@gmail.com>
2023-05-24 09:06:16 +0200

    m

--- src/Components/cards/Card.js
+++ src/Components/cards/Card.js
@@ -5,19 +5,19 @@ export default function Card({ img, title, extract, slug }) {
   return (
     <>
         <div className="mx-auto  items-center grid grid-cols-1  rounded-md my-5  shadow-xl hover:shadow-2xl">
-      <Link href={`post/${slug}`}>
           <div>
+      <Link href={`post/${slug}`}>
             <img
               src={img}
               alt={title}
               className="w-96 h-48  object-cover rounded-xl"
             />
+      </Link>
           </div>
           <div className="text-left pl-8">
             <p className="pt-1 max-w-sm">{title}</p>
             <p className="pb-2 max-w-sm">{extract}</p>
           </div>
-      </Link>
         </div>
     </>
   );
